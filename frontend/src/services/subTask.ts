c7b63c360853147a019b788d7e59df77f112d868
IshaqKam <ishkam2001@gmail.com>
2023-07-03 09:38:22 +0500

    fix: add subtask service added

--- frontend/src/services/subTask.ts
+++ frontend/src/services/subTask.ts
@@ -9,12 +9,31 @@ export async function UpdateSubtaskStatus({
   completed: boolean;
 }) {
   try {
-    const response = await axios.put(
+    const response = await axios.patch(
       `${API_BASE_URL}/api/subtask/${subtaskId}`,
       {
         completed,
       }
     );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function AddSubtask({
+  title,
+  todoId,
+}: {
+  title: string;
+  todoId: string;
+}) {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/api/subtask/`, {
+      title,
+      todoId,
+    });
     return response.data;
   } catch (error) {
     console.error(error);
